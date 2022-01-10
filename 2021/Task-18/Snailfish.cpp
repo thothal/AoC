800b8d94ecb24ece82a3f1dee1821377936a1518
Thorn Thaler <foren@thothal.com>
2022-01-10 18:05:12 +0100

    Move Logic from Snailfish to Node

--- 2021/Task-18/Snailfish.cpp
+++ 2021/Task-18/Snailfish.cpp
@@ -4,19 +4,16 @@
 using namespace Rcpp;
 
 enum Position {
-	root,
 	left,
-	right};
+	right,
+	root
+};
 
-class Node {
+class Node: public std::enable_shared_from_this<Node> {
 public:
-	Node() {
-		this->set_depth();
-	};
 	
-	Node(const int value, Node* parent): value_(value) {
-		this->parent_ = std::shared_ptr<Node>(parent);
-		this->set_depth();
+	[[nodiscard]] static std::shared_ptr<Node> create() {
+		return std::shared_ptr<Node>(new Node());
 	};
 	
 	bool is_regular() const {
@@ -27,257 +24,259 @@ public:
 		return !this->parent_.lock();
 	};
 	
-	unsigned int depth() const {
-		return this->depth_;
+	std::shared_ptr<Node> ptr() {
+		return shared_from_this();
 	};
-private:
-	int value_;
-	unsigned int depth_;
-	std::weak_ptr<Node> parent_;
-	std::shared_ptr<Node> left_;
-	std::shared_ptr<Node> right_;
 	
-	Node* set_depth() {
+	unsigned int depth() {
+		unsigned int depth = 0;
 		std::shared_ptr<Node> pa = this->parent_.lock();
-		if (pa) {
-			this->depth_ = pa->depth_ + 1;
+		while (pa) {
+			depth++;
+			pa = pa->parent_.lock();
+		}
+		return depth;
+	}
+	
+	int value() const {
+		return this->value_;
+	};
+	
+	Position position() {
+		if (!this->is_root()) {
+			std::shared_ptr<Node> pa = this->parent_.lock();
+			if (this->ptr() == pa->left_) {
+				return Position::left;
+			} else {
+				return Position::right;
+			}
 		} else {
-			this->depth_ = 0;
+			return Position::root;
 		}
-		return this;
-	};
-};
-
-/*
-struct Node {
-	int value;
-	int depth;
-	Position pos;
-	bool regular;
-	
-	std::weak_ptr <Node> parent;
-	std::shared_ptr <Node> left;
-	std::shared_ptr <Node> right;
-	
-	Node(): depth(0) {};
-	
-	Node(int value_, int depth_, Position pos_) 
-		: value(value_), depth(depth_), pos(pos_), regular(true) {};
-	
-	std::shared_ptr <Node> child(Position dir) {
-		return (dir == Position::left) ? left : right;
-	}
-};
-
-std::ostream& operator<<(std::ostream& os, const Node& rhs) {
-	if (rhs.regular) {
-		os << rhs.value;
-	} else {
-		os << "[" << *rhs.left << "," << *rhs.right << "]";
-	}
-	return os;
-};
-
-class Snailfish {
-public:
-	Snailfish(List sf) {
-		root = std::make_shared<Node>();
-		root->pos = Position::root;
-		root->regular = false;
-		root->depth = 0;
-		breed_snailfish(sf, root);
 	};
 	
-	friend std::ostream& operator<<(std::ostream& os, const Snailfish& rhs) {
-		os << *rhs.root;
-		return os;	
+	std::shared_ptr<Node> set_value(const int value) {
+		this->value_ = value;
+		return this->ptr();
 	};
-	//private:
-	std::shared_ptr<Node> root;
 	
-	void breed_snailfish(List sf, std::weak_ptr<Node> parent) {
-		Rcout << *parent.lock().get() <<":" << parent.lock().use_count() << std::endl;
-		if (sf.length() != 2) {
-			stop("'sf' must be a list of length 2");
+	std::shared_ptr<Node> set_child(std::shared_ptr<Node> kid, const Position pos) {
+		kid->parent_ = this->ptr();
+		if (pos == Position::left) {
+			this->left_ = kid;
+		} else {
+			this->right_ = kid;
 		}
-		SEXP left_el  = sf[0],
-                    right_el = sf[1];
-		SEXPTYPE left_type  = TYPEOF(left_el),
-			right_type = TYPEOF(right_el);
-		std::shared_ptr<Node> left_node = std::make_shared<Node>();
-		left_node->pos = Position::left;
-		left_node->parent = parent;
-		left_node->regular = false;
-		left_node->depth = parent.lock()->depth + 1;
-		
-		std::shared_ptr<Node> right_node = std::make_shared<Node>();
-		right_node->pos = Position::right;
-		right_node->parent = parent;
-		right_node->regular = false;
-		right_node->depth = parent.lock()->depth + 1;
-		
-		parent.lock()->left = left_node;
-		parent.lock()->right = right_node;
-		if (left_type == INTSXP) {
-			// leaf node
-			left_node->value = *INTEGER(left_el);
-			left_node->regular = true;
-		} else if (left_type == REALSXP){
-			left_node->value = (int) *REAL(left_el);
-			left_node->regular = true;
-		} else if (left_type == VECSXP) {
-			breed_snailfish(left_el, left_node); 
-		}
-		if (right_type == INTSXP) {
-			// leaf node
-			right_node->value = *INTEGER(right_el);
-			right_node->regular = true;
-		} else if (right_type == REALSXP){
-			right_node->value = (int) *REAL(right_el);
-			right_node->regular = true;
-		} else if (right_type == VECSXP) {
-			breed_snailfish(right_el, right_node); 
-		}
+		return this->ptr();
+	};
+	
+	std::shared_ptr<Node> set_childs(const std::array<std::shared_ptr<Node>, 2>& kids) {
+		this->set_child(kids[0], Position::left);
+		this->set_child(kids[1], Position::right);
+		return this->ptr();
 	};
 	
 	std::shared_ptr<Node> next() {
 		bool exploded = false;
-		return walk(root, exploded);
+		return this->next(exploded);
 	};
-	
-	std::shared_ptr<Node> walk(std::shared_ptr<Node> me, bool &exploded) {
-		std::shared_ptr<Node> node, node2;
-		if (!exploded) {
-			if (me->regular) {
-				if (me->value >= 10) {
-					node = me;
-				}
-			} else {
-				if (me->depth == 4) {
-					exploded = true;
-					node = me;
-				} else {
-					node = walk(me->left, exploded);
-					if (!exploded) {
-						node2 = walk(me->right, exploded);
-					}
-					if (!node) {
-						node = node2;
-					}
-				}
-			}
+
+	Node* explode() {
+		std::shared_ptr<Node> left_nb = neighbor(Position::left);
+		std::shared_ptr<Node> right_nb = neighbor(Position::right);
+		int left_val = this->left_->value_,
+			right_val = this->right_->value_;
+		if (left_nb) {
+			left_nb->value_ += left_val;
 		}
-		return node;
+		if (right_nb) {
+			right_nb->value_ += right_val;
+		}
+		this->left_.reset();
+		this->right_.reset();
+		this->value_ = 0;
+		return this;
 	};
 	
-	std::shared_ptr<Node> neighbor(std::shared_ptr<Node> start, Position dir) {
-		std::shared_ptr<Node> me = start;
-		std::shared_ptr<Node> pa = start->parent.lock();
-		while (pa && me->pos == dir) {
+	Node* split() {
+		int val = this->value_;
+		this->left_ = Node::create();
+		this->left_->set_value(floor(val / 2.0));
+		this->right_ = Node::create();
+		this->right_->set_value(ceil(val / 2.0));
+		return this;
+	};
+	
+	friend std::ostream& operator<<(std::ostream& os, const Node& rhs);
+private:
+	// Do not allow direct init to force use of create
+	Node() = default;
+	int value_;
+	std::weak_ptr<Node> parent_;
+	std::shared_ptr<Node> left_;
+	std::shared_ptr<Node> right_;
+	
+	std::shared_ptr<Node> neighbor(const Position dir) {
+		std::shared_ptr<Node> me = this->ptr();
+		std::shared_ptr<Node> pa = this->parent_.lock();
+		while (pa && me->position() == dir) {
 			me = pa;
-			pa = pa->parent.lock();
+			pa = pa->parent_.lock();
 		}
 		if (pa) {
 			Position new_dir = dir;
-			while (!pa->regular) {
-				pa = pa->child(new_dir);
+			while (!pa->is_regular()) {
+				if (new_dir == Position::left) {
+					pa = pa->left_;
+				} else {
+					pa = pa->right_;
+				}
 				new_dir = dir == Position::left ? Position::right : Position::left;
 			}
 		}
 		return pa;
 	};
 	
-	Snailfish* explode(std::shared_ptr<Node> node) {
-		std::shared_ptr<Node> left_nb = neighbor(node, Position::left);
-		std::shared_ptr<Node> right_nb = neighbor(node, Position::right);
-		int left_val  = node->left->value,
-			right_val = node->right->value;
-		if (left_nb) {
-			left_nb->value += left_val;
-		}
-		if (right_nb) {
-			right_nb->value += right_val;
-		}
-		node->left.reset();
-		node->right.reset();
-		node->value = 0;
-		node->regular = true;
-		return this;
-	};
-	
-	Snailfish* split(std::shared_ptr<Node> node) {
-		int val = node->value;
-		node->left  = std::make_shared<Node>(floor(val / 2.0),
-                                       node->depth + 1, Position::left);
-		node->right = std::make_shared<Node>(ceil(val / 2.0),
-                                       node->depth + 1, Position::right);
-		node->left->parent = node;
-		node->right->parent = node;
-		node->regular = false;
-		return this;
-	};
-	
-	Snailfish* reduce() {
-		std::shared_ptr<Node> next_node = next();
-		while (next_node) {
-			if (next_node->regular) {
-				split(next_node);
+	std::shared_ptr<Node> next(bool& exploded) {
+		std::shared_ptr<Node> node;
+		if (!exploded) {
+			if (this->is_regular()) {
+				if (this->value() >= 10) {
+					node = this->ptr();
+				}
 			} else {
-				explode(next_node);
+				if (this->depth() == 4) {
+					exploded = true;
+					node = this->ptr();
+				} else {
+					node = this->left_->next(exploded);
+					if (!exploded && !node) {
+						node = this->right_->next(exploded);
+					}
+				}
 			}
-			Rcout << *root << std::endl;
-			next_node = next();
 		}
+		return node;
+	};
+};
+
+std::ostream& operator<<(std::ostream& os, const Node& rhs) {
+	if (rhs.is_regular()) {
+		os << rhs.value();
+	} else {
+		os << "[" << *rhs.left_ << "," << *rhs.right_ << "]";
+	}
+	return os;
+};
+
+class Snailfish {
+public:
+	
+	Snailfish* breed_snailfish(List sf) {
+		this->root_ = Node::create();
+		this->breed_snailfish(sf, this->root_);
 		return this;
 	};
 	
-	void reset_depth(std::shared_ptr<Node> node) {
-		if (node) {
-			if (node->parent.lock()) {
-				node->depth = node->parent.lock()->depth + 1;
-			}
-			reset_depth(node->left);
-			reset_depth(node->right);
-		}
+	std::shared_ptr<Node> root() {
+		return root_;
 	}
 	
+	Snailfish* reduce() {
+		std::shared_ptr<Node> next_node = this->root_->next();
+		while (next_node) {
+			if (next_node->is_regular()) {
+				next_node->split();
+			} else {
+				next_node->explode();
+			}
+			Rcout << *root_ << std::endl;
+			next_node = this->root_->next();
+		}
+		return this;	
+	};
+	
 	Snailfish& operator+=(const Snailfish& rhs) {
-		std::shared_ptr<Node> new_root = std::make_shared<Node>();
-		new_root->pos = Position::root;
-		
-		root->pos = Position::left;
-		root->parent = new_root;
-		
-		rhs.root->pos = Position::right;
-		rhs.root->parent = new_root;
-		
-		new_root->left = root;
-		new_root->right = rhs.root;
-		
-		Rcout << *root << "\t"<< *new_root << std::endl;
-		root = new_root;
-		reset_depth(root);
+		std::shared_ptr<Node> new_root = Node::create();
+		std::array<std::shared_ptr<Node>, 2> kids = {this->root_, rhs.root_};
+		new_root->set_childs(kids);
+		this->root_ = new_root;
 		return *this;
 	}
 	
-	friend Snailfish operator+(Snailfish lhs, const Snailfish& rhs) {
-		lhs += rhs; // reuse compound assignment
-		return lhs; // return the result by value (uses move constructor)
-	}
-};*/
+	friend std::ostream& operator<<(std::ostream& os, const Snailfish& rhs);
+
+private:
+	std::shared_ptr<Node> root_;
+	
+	void breed_snailfish(List sf, std::shared_ptr<Node> parent) {
+		if (sf.length() != 2) {
+			stop("'sf' must be a list of length 2");
+		}
+		SEXP left_el = sf[0],
+                   right_el = sf[1];
+		SEXPTYPE left_type  = TYPEOF(left_el),
+			right_type = TYPEOF(right_el);
+		std::shared_ptr<Node> left_node = Node::create();
+		std::shared_ptr<Node> right_node = Node::create();
+		
+		if (left_type == INTSXP) {
+			// leaf node
+			left_node->set_value(*INTEGER(left_el));
+		} else if (left_type == REALSXP){
+			left_node->set_value((int) *REAL(left_el));
+		} else if (left_type == VECSXP) {
+			breed_snailfish(left_el, left_node); 
+		}
+		
+		if (right_type == INTSXP) {
+			// leaf node
+			right_node->set_value(*INTEGER(right_el));
+		} else if (right_type == REALSXP){
+			right_node->set_value((int) *REAL(right_el));
+		} else if (right_type == VECSXP) {
+			breed_snailfish(right_el, right_node); 
+		}		
+		std::array<std::shared_ptr<Node>, 2> kids = {left_node, right_node};
+		parent->set_childs(kids);
+	};
+};
+
+std::ostream& operator<<(std::ostream& os, const Snailfish& rhs) {
+	os << *rhs.root_;
+	return os;
+};
+
+Snailfish operator+(Snailfish lhs, const Snailfish& rhs) {
+	lhs += rhs;
+	return lhs;
+}
+
+/*
+
+ void reset_depth(std::shared_ptr<Node> node) {
+ if (node) {
+ if (node->parent.lock()) {
+ node->depth = node->parent.lock()->depth + 1;
+ }
+ reset_depth(node->left);
+ reset_depth(node->right);
+ }
+ }
+ 
+ */
 
 // [[Rcpp::export]]
-void snailfish(List a, List b) {
-	Node a_(1, nullptr);
-	Node b_(1, &a_);
-	Rcout << std::boolalpha << a_.is_regular() << "|" << a_.is_root() << std::endl;
-	Rcout << std::boolalpha << b_.is_regular() << "|" << b_.is_root() << std::endl;
-	/*Snailfish sf(a);
-	Rcout << sf.root->left->left.use_count();
+void snailfish(const List& a, const List& b) {
+	Snailfish sf1, sf2;
+	sf1.breed_snailfish(a);
+	sf2.breed_snailfish(b);
+	Rcout << *(sf1 + sf2).reduce() << std::endl;
 	//Rcout << *sfs.explode(sfs.next()) << std::endl;
 	//delete sf;
-	//delete sf1;*/
+	//delete sf1;
 };
+
 
 /*** R
 ## Rcpp::sourceCpp(here::here("2021", "Task-18", "Snailfish.cpp"))
